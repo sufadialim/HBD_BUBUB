7ec66eec39e22e42800fbd4ab1231b5ddb77d180
Sufadi Alim <129618274+sufadialim@users.noreply.github.com>
2025-09-09 22:57:40 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,15 @@
 /** @type {import('next').NextConfig} */
-
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
   output: 'export',
-  basePath: isProd ? "/HBD_BUBUB" : "",
-  assetPrefix: isProd ? "/HBD_BUBUB/" : "",
+  trailingSlash: true,
+  basePath: '/HBD_BUBUB',
+  assetPrefix: '/HBD_BUBUB/',
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
-  trailingSlash: true,
   images: {
     unoptimized: true,
   },
